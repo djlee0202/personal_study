fffb0f88071340f738b0ec1f224c719ef9c8ecdc
djlee0202 <djlee0202@gmail.com>
2016-11-04 11:15:15 -0400

    updated_11_4

--- animal_cafe/slides.js
+++ animal_cafe/slides.js
@@ -3,15 +3,20 @@
     height = 800 - margin.top - margin.bottom,
     width = 1200 - margin.left - margin.right;
 
-  var svg = d3.select("#graphic")
+  var root = d3.select("#graphic")
         .append("svg")
         .attr("height", height + margin.top + margin.bottom)
         .attr("width", width + margin.left + margin.right)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  var map = svg.append("g").attr("id", "map"),
-      places = svg.append("g").attr("id", "places");
+  var svg = root.append("g");
+
+  var map = svg.append("g")
+               .attr("id", "map")
+
+  var places = svg.append("g")
+                  .attr("id", "places");
 
   var projection = d3.geo.mercator()
       .center([126.9895, 37.5651])
@@ -20,14 +25,18 @@
 
   var path = d3.geo.path().projection(projection);
 
-  d3.json("seoul_municipalities_topo_simple.json", function(error, data) {
+  d3.json("seoul_municipalities_topo_simple.json",
+
+  function(error, data) {
     var features = topojson.feature(data, data.objects.seoul_municipalities_geo).features;
     map.selectAll('path')
-        .data(features)
-      .enter().append('path')
-        .attr('class', function(d) { console.log(); return 'municipality c' + d.properties.code })
-        .attr('d', path)
-        .style("opacity", 0) ;
+       .data(features)
+       .enter().append('path')
+       .attr('class', function(d) {
+          // console.log();
+          return 'municipality c' + d.properties.code })
+       .attr('d', path)
+       .style("opacity", 0) ;
 
 
     map.selectAll('text')
@@ -66,12 +75,21 @@
 
   // for inspiration:
   // hue, 0-360
-  var angleScaleHue = d3.scaleLinear().domain([0, 360]).range([0, Math.PI * 2]);
-  // lightness, 0-100
-  // saturation, 0-100
-  var radius = 200;
-  var radiusScaleLightness = d3.scaleLinear().domain([0, 100]).range([0, radius]);
-  var radiusScaleSaturation = d3.scaleLinear().domain([0, 100]).range([0, radius]);
+  var lineScale = d3.scaleLinear().domain([1, 15]).range([0, 'height']);
+
+  var defs = places.append('defs');
+
+  defs.append("pattern")
+      .attr("id","jon-snow")
+      .attr("height", "100%")
+      .attr("width", "100%")
+      .attr("patternContentUnits", "objectBoundingBox")
+      .append("image")
+      .attr("height", 1)
+      .attr("width", 1)
+      .attr("preserveAspectRatio", "none")
+      .attr("xmlns:xlink", "http://www.w3.org/1999/xlink")
+      .attr("xlink:href", "snow.jpg")
 
   function ready(error, datapoints) {
 
@@ -99,68 +117,83 @@
   // 2) make them all radius 4
   // 3) move them to the center of the svg
 
-  
 
 
-    var config = {"avatar_size" : 48}
-    var defs = places.append('places:defs');
 
-    defs.append("places:pattern")
-        .attr("id", "grump_avatar")
-        .attr("width", config.avatar_size)
-        .attr("height", config.avatar_size)
-        .attr("patternUnits", "userSpaceOnUse")
-        .append("svg:image")
-        .attr("xlink:href", 'http://placekitten.com/g/48/48')
-        .attr("width", config.avatar_size)
-        .attr("height", config.avatar_size)
+
+
+    // var images = defs.selectAll(".cafe-pattern")
+    //                  .data(datapoints)
+    //                  .enter().append("pattern")
+    //                  .attr("class", "cafe-pattern")
+    //                  .attr("height", "100%")
+    //                  .attr("width", "100%")
+    //                  .attr("id", function(d) {
+    //                    // everyone has a unique name, so this sould be okay
+    //                    return d.name.toLowerCase().replace(/ /g, "-")
+    //                  })
+    //                   .attr("patternContentUnits", "objectBoundingBox")
+    //                   .append("image")
+    //                   .attr("height", 48)
+    //                   .attr("width", 48)
+    //                   .attr("preserveAspectRatio", "none")
+    //                   .attr("xmlns:xlink", "http://www.w3.org/1999/xlink")
+    //                   .attr("xlink:href", 'http://placekitten.com/g/48/48')
+    //                   .attr("x", function(d) { return -25;})
+    //                   .attr("y", function(d) { return -25;})
+
         ;
 
-    var another_circle = places.selectAll(".colordot")
-      // .filter(function(d) {
-      //   return d.name_chinese != "";
-      // })
-      .attr("cx", function(d) { return projection([d.lon, d.lat])[0]; })
-      .attr("cy", function(d) { return projection([d.lon, d.lat])[1]; })
-      .attr("r", 0)
-      .style("fill", "#fff")
+    var colordots = places.selectAll(".colordot")
+                          .attr("cx", function(d) { return projection([d.lon, d.lat])[0]; })
+                          .attr("cy", function(d) { return projection([d.lon, d.lat])[1]; })
+                          .attr("r", 0)
+                          .style("fill", "#fff")
+                          .style("fill", "url(#jon-snow)");
 
-
+    //
     places.selectAll(".colortext")
-      .data(datapoints)
-      .enter().append("text")
-      .attr("class", "colortext")
-      .attr("x", function(d) { return projection([d.lon, d.lat])[0]; })
-      .attr("y", function(d) { return projection([d.lon, d.lat])[1]; })
-      .text(function(d) {
-        return d.name
-      })
-      .attr("dy", 2)
-      .attr("dx", 2)
-      .style("opacity", 0)
+          .data(datapoints)
+          .enter().append("text")
+          .attr("class", "colortext")
+          .attr("x", function(d) { return projection([d.lon, d.lat])[0]; })
+          .attr("y", function(d) { return projection([d.lon, d.lat])[1]; })
+          .text(function(d) {return d.name})
+          .attr("dy", 5)
+          .attr("dx", 20)
+          .style("opacity", 0)
 
 
     // 사진 넣기
 
 
 
-      var another_circle = places.append("circle")
-        .attr("cx", config.avatar_size/2)
-        .attr("cy", config.avatar_size/2)
-        .attr("r", config.avatar_size/2)
-        .style("fill", "#fff")
-        .style("fill", "url(#grump_avatar)")
-        .attr("x", 0)
-        .attr("y", 0);
-
-
       // var setEvents = images
       //         // Append hero text
       //         .on( 'click', function (d) {
-      //             d3.select("h1").html(d.hero);
-      //             d3.select("h2").html(d.name);
-      //             d3.select("h3").html ("Take me to " + "<a href='" + d.link + "' >"  + d.hero + " web page ⇢"+ "</a>" );
+      //             d3.select("h1").html("A");
+      //             d3.select("h2").html("B");
+      //             d3.select("h3").html ("C" );
       //          })
+      //
+      //          .on( 'mouseenter', function() {
+      //            // select element in current context
+      //            d3.select( this )
+      //              .transition()
+      //              .attr("x", function(d) { return -60;})
+      //              .attr("y", function(d) { return -60;})
+      //              .attr("height", 100)
+      //              .attr("width", 100);
+      //          })
+      //          // set back
+      //          .on( 'mouseleave', function() {
+      //            d3.select( this )
+      //              .transition()
+      //              .attr("x", function(d) { return -25;})
+      //              .attr("y", function(d) { return -25;})
+      //              .attr("height", 50)
+      //              .attr("width", 50);
+      //          });
       //
       //         .on( 'mouseenter', function() {
       //           // select element in current context
@@ -190,6 +223,9 @@
     // console.log("Just slid into slide 2")
     // console.log("Just slid out of slide 4")
 
+
+
+// Slide 2--------------------------------------------------------------------
     d3.select("#slide-1").on('slidein', function() {
     places.selectAll(".colortext")
     .transition()
@@ -204,7 +240,7 @@
     places.selectAll(".colordot")
     .transition()
     .duration(1200)
-    .attr("r", 10)
+    .attr("r", 20)
     .style("opacity", .5)
 
     })
@@ -214,9 +250,10 @@
       .transition()
       .duration(2500)
       .style("opacity", 0)
-      console.log("Just slid out of slide 1")
     })
 
+
+// Slide 2--------------------------------------------------------------------
     d3.select("#slide-2").on('slidein', function() {
       // get all of the color dots!
       // then filter to only the ones with a chinese name
@@ -224,15 +261,15 @@
       places.selectAll(".colordot")
       .transition()
       .duration(2500)
-      .attr("r", 5)
-      .attr("cx", function(d, i) {return i % 6 * 80;})
-      .attr("cy", function(d, i) {return parseInt(i / 6) * 18;})
+      .attr("r", 20)
+      .attr("cx", function(d, i) {return i % 2 * 80;})
+      .attr("cy", function(d, i) {return parseInt(i / 30) * 20;})
 
       places.selectAll(".colortext")
       .transition()
       .duration(2500)
-      .attr("x", function(d, i) {return i % 6 * 80;})
-      .attr("y", function(d, i) {return parseInt(i / 6) * 18;})
+      .attr("x", function(d, i) {return i % 2* 80;})
+      .attr("y", function(d, i) {return parseInt(i / 10) * 200;})
       .attr("dy", 5)
       .attr("dx", 15)
     })
@@ -241,6 +278,9 @@
       // console.log("Just slid out of slide 2")
     })
 
+
+
+// Slide 3--------------------------------------------------------------------
     d3.select("#slide-3").on('slidein', function() {
       // get all of the color dots!
       // then filter to only the ones with a chinese name
@@ -271,63 +311,14 @@
       // console.log("Just slid out of slide 3")
     })
 
+// Slide 4--------------------------------------------------------------------
     d3.select("#slide-4").on('slidein', function() {
-
-      // get all of the color dots!
-      // then filter to only the ones with a english name
-      // a.k.a. "if your name_english is not an empty string"
-      svg.selectAll(".colordot")
-        .transition()
-        .duration(750)
-        .attr("r", function(d) {
-          if(d.name_english != "") {
-            return 2;
-          } else {
-            return 0;
-          }
-        })
-        .attr("cx", function(d) {
-          var a = angleScaleHue(d.Hue);
-          var r = radiusScaleLightness(d.Lightness);
-          return r * Math.sin(a) + width / 2;
-        })
-        .attr("cy", function(d) {
-          var a = angleScaleHue(d.Hue);
-          var r = radiusScaleLightness(d.Lightness);
-          return r * Math.cos(a) + height / 2;
-        })
-
 
     })
 
     d3.select("#slide-4").on('slideout', function() {
       // console.log("Just slid out of slide 4")
     })
-
-    d3.select("#slide-5").on('slidein', function() {
-      svg.selectAll(".colordot")
-        .transition()
-        .duration(750)
-        .attr("r", function(d) {
-          if(d.name_english != "") {
-            return 2;
-          } else {
-            return 0;
-          }
-        })
-        .attr("cx", function(d) {
-          var a = angleScaleHue(d.Hue);
-          var r = radiusScaleSaturation(d.Saturation);
-          return r * Math.sin(a) + width / 2;
-        })
-        .attr("cy", function(d) {
-          var a = angleScaleHue(d.Hue);
-          var r = radiusScaleSaturation(d.Saturation);
-          return r * Math.cos(a) + height / 2;
-        })
-    })
-
-
 
 
   }
